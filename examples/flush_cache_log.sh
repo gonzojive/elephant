136b6b22abf70d86cdaf644ed8b72c512c97d8ef
alex.mizrahi <alex.mizrahi@gmail.com>
2007-09-20 02:58:06 -0700

    added sh script for flushing logs sample
    darcs-hash:20070920095806-cd2f4-628857ab5409a14180c008ddae11392e3598d0b8.gz

---
+++ examples/flush_cache_log.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#clears logs used for caching in db-postmodern backend (when *cache-mode* is :global-sync-cache)
+#replace "elepm" with a working database name and "610" with a value of max-resync-time if non-standard is used
+
+psql -c "DELETE FROM transaction_log WHERE commit_time < (extract(epoch from current_timestamp) - 610);" elepm
+psql -c "DELETE FROM update_log WHERE txn_id NOT IN (SELECT txn_id FROM transaction_log);" elepm
+
+#this script might be suboptimal in case of large number of transactions.
+#possibly a better one:
+# BEGIN;
+# DELETE FROM update_log USING transaction_log WHERE (update_log.txn_id = transaction_log.txn_id) AND 
+# DELETE FROM transaction_log WHERE commit_time < (extract(epoch from current_timestamp) - 610);
+# COMMIT;
+
+
