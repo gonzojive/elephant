6cfb92837911ee2d8ee5ce7cca97e76c93cc9924
Henrik Hjelte <henrik@evahjelte.com>
2007-11-01 03:26:27 -0700

    POSTMODERN-tests include hints to configure postgres
    darcs-hash:20071101102627-7d60f-c15b471a776b42c80e86ec077c3138f505d0130f.gz

--- tests/POSTMODERN-tests.lisp
+++ tests/POSTMODERN-tests.lisp
@@ -19,13 +19,52 @@
 
 ;; Note:  One almost certainly has to execute "createdb elepm" as the user "postgres"
 ;; in order for this to work.
-;; The Postemodern interface also apparently requries that "plpgsql language be loaded as 
+;; The Postmodern interface also requries that "plpgsql language be loaded as 
 ;; a query language into the postgres instance.  This can be created by executing
 ;; this in a shell:
 ;;     psql -c 'create language plpgsql' elepm;
+
 (defparameter *testpm-spec* '(:postmodern (:postgresql "127.0.0.1" "elepm" "postgres" "")))
 
 (setf *default-spec* *testpm-spec*)
 
 (do-backend-tests)
 
+;; Hints for configuring postgresql (Unix biased)
+;; ==============================================
+;;       As root, switch to the postgres user
+;;       
+;;           su postgres 
+;;       
+;;       Configure pg_hba.conf. As postgres user, edit /var/lib/pgsql/data/pg_hba.conf to 
+;;       allow password logins.The change is 'md5' instead of 'ident sameuser' in two places
+;;           
+;;           # TYPE DATABASE USER CIDR-ADDRESS METHOD 
+;;           
+;;           # "local" is for Unix domain socket connections only 
+;;           
+;;           local all all ident sameuser 
+;;           
+;;           # IPv4 local connections: 
+;;           
+;;           host all all 127.0.0.1/32 md5 
+;;           
+;;           # IPv6 local connections: 
+;;           
+;;           host all all ::1/128 md5 
+;;       
+;;       Create database, as postgres user.
+;;       
+;;           su postgres
+;;           psql -c "create user myuser with password 'mypassword';"
+;;
+;;      Now you should be able to have run under a specific user:
+;;      (defparameter *sample-postmodern-spec* '(:postmodern (:postgresql "127.0.0.1" "elepm" "myuser" "mypassword")))
+
+;; Convenient kill/yank to remake a database, run as postgres user;
+;;    "
+;;     dropdb elepm;
+;;     createdb elepm;
+;;     psql -c 'grant all on database elepm to myuser;' postgres;
+;;     psql -c 'create language plpgsql' elepm;
+;;    "
