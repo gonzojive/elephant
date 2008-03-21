d57a9dc9bd2bac67192e24997ae87ac6af59e160
eslick <eslick@common-lisp.net>
2008-03-21 03:46:48 -0700

    Better explicit vs. slot association test
    darcs-hash:20080321104648-56764-356fde2e026ea962bbfa10ac3d184817efc17ddf.gz

--- tests/testassociations.lisp
+++ tests/testassociations.lisp
@@ -2,12 +2,16 @@
 
 (in-suite* testassociations :in elephant-tests)
 
+(defparameter NUM_JOBS 100)
+(defparameter NUM_PERSONS 10000)
+(defparameter PERSONS_PER_JOB 100)
+
 ;; Create a one-to-many association from job to person...
 ;; one job can have up to 10 persons, in any order.  
 ;; In this test I will do this with an explict association class,
 ;; and then maybe Ian and the Marc, the new guy, can beat it 
 ;; with an explicit association....
-(test simple-explicit-assoc
+(test simple-explicit-assoc-setup
   (when (find-class 'person nil)
     (drop-instances (get-instances-by-class 'person) :txn-size 500))
   (when (find-class 'job nil)
@@ -26,15 +30,8 @@
    ((person :accessor person-of :initarg :person :index t)
     (job :accessor job-of :initarg :job :index t)))
 
- (flet ((find-all-persons-by-job (j)
-   (get-instances-by-value (find-class 'pjassoc) 'job (elephant::oid
-j))))
-
- (let ((NUM_JOBS 100)
-	(NUM_PERSONS 10000)
-	(PERSONS_PER_JOB 100)
-	(job-oids nil)
-	(person-oids nil))
+ (let ((job-oids nil)
+       (person-oids nil))
    (with-transaction ()
      (dotimes (x NUM_JOBS)
        (push (elephant::oid (make-instance 'job  :title (string (gensym)))) job-oids)))
@@ -48,16 +45,24 @@ j))))
 	 (let ((p (nth (random NUM_PERSONS) person-oids))
 	       (j (nth (random NUM_JOBS) job-oids)))
 	   (make-instance 'pjassoc :person p :job j)))))
-   ;; do a test here... 
-   (let ((total 0))
-     (map-class #'(lambda (job) 
-		    (incf total (length (find-all-persons-by-job job))))
+   (is (eq t t))))
+
+
+(defun count-explicit-persons ()
+  (flet ((find-all-persons-by-job (j)
+	   (get-instances-by-value (find-class 'pjassoc) 'job (elephant::oid j))))
+    (let ((total 0))
+      (map-class #'(lambda (job) 
+		     (incf total (length (find-all-persons-by-job job))))
 		 (find-class 'job))
-     (is (= total (* NUM_JOBS PERSONS_PER_JOB)))))))
+      total)))
+
+(test (simple-explicit-assoc :depends-on simple-explicit-assoc-setup)
+      (is (= (count-explicit-persons) (* NUM_JOBS PERSONS_PER_JOB))))
 
 
 
-(test simple-slot-assoc
+(test simple-slot-assoc-setup
   (when (find-class 'person nil)
     (drop-instances (get-instances-by-class 'person) :txn-size 500))
   (when (find-class 'job nil)
@@ -72,10 +77,7 @@ j))))
     (company :accessor company-of :initarg :company)
     (holders :accessor holders :associate (person jobs) :many-to-many t)))
 
- (let ((NUM_JOBS 100)
-       (NUM_PERSONS 10000)
-       (PERSONS_PER_JOB 100)
-       (jobs nil)
+ (let ((jobs nil)
        (people nil))
    (with-transaction ()
      (dotimes (x NUM_JOBS)
@@ -91,12 +93,23 @@ j))))
 	 (let ((p (nth (+ k (* i PERSONS_PER_JOB)) people))
 	       (j (nth i jobs)))
 	   (setf (jobs p) j)))))
-   ;; do a test here... 
-   (let ((total 0))
-     (map-class #'(lambda (job) 
-                      (incf total (length (holders job))))
-		'job)
-     (is (= total (* NUM_JOBS PERSONS_PER_JOB))))))
+   (is (eq t t))))
+
+(defun count-slot-persons ()
+  (let ((total 0))
+    (map-class #'(lambda (job) 
+		   (incf total (length (holders job))))
+	       'job)
+    total))
+
+(defun count-persons ()
+  (let ((total 0))
+    (map-class #'(lambda (person) (incf total)) 
+	       'person :oids t)
+    total))
+
+(test (simple-slot-assoc :depends-on simple-slot-assoc-setup)
+  (is (= (count-slot-persons) (* NUM_JOBS PERSONS_PER_JOB))))
   
 
 (defun assoc-timing-comparison ()
@@ -107,9 +120,17 @@ j))))
   (when (find-class 'pjassoc nil)
     (drop-instances (get-instances-by-class 'pjassoc) :txn-size 100))
 
- (time (do-test 'simple-explicit-assoc))
+ (format t "~%~%Explicit assoc setup~%")
+ (time (do-test 'simple-explicit-assoc-setup))
+ (format t "~%~%Explicit assoc retrieve~%")
+ (elephant::flush-instance-cache *store-controller*)
+ (time (count-explicit-persons))
 
  (drop-instances (get-instances-by-class 'person) :txn-size 500)
  (drop-instances (get-instances-by-class 'job) :txn-size 500)
 
- (time (do-test 'simple-slot-assoc)))
+ (format t "~%~%Slot assoc setup~%")
+ (time (do-test 'simple-slot-assoc-setup))
+ (format t "~%~%Slot assoc retrieve~%")
+ (elephant::flush-instance-cache *store-controller*)
+ (time (count-slot-persons)))
